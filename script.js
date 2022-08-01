aa61d8b7892ff79e40ef912c57bc3e17a5d354e4
hamzashaukat111 <75229817+hamzashaukat111@users.noreply.github.com>
2022-08-01 14:48:18 +0500

    Added game functionality

--- script.js
+++ script.js
@@ -11,12 +11,38 @@ const option = ['rock', 'paper', 'Scissor'];
 
 //user chooses option
 //document.getElementById
-document.getElementById('rock').addEventListener('click', function () {});
+const dhekh = document.querySelector('.dhekho');
+const rockConst = document.getElementById('ROCK');
+const paperConst = document.getElementById('PAPER');
+const scissorConst = document.getElementById('SCISSOR');
+
+//chpsing Rock
+document.getElementById('rock').addEventListener('click', function () {
+  dhekh.classList.remove('hidden');
+  rockConst.classList.remove('hidden');
+  playerSelection = 'rock';
+  play(playerSelection, computerSelection);
+});
+
+//chosing paper
+document.getElementById('paper').addEventListener('click', function () {
+  dhekh.classList.remove('hidden');
+  paperConst.classList.remove('hidden');
+  playerSelection = 'paper';
+  play(playerSelection, computerSelection);
+});
+
+//chosing Scissor
+document.getElementById('Scissor').addEventListener('click', function () {
+  dhekh.classList.remove('hidden');
+  scissorConst.classList.remove('hidden');
+  playerSelection = 'Scissor';
+  play(playerSelection, computerSelection);
+});
 //user
 
 //computer choses
 function comp() {
-  let CHOICE = Math.trunc(Math.random() * 3);
   ruk = Math.trunc(Math.random() * 3);
   computerSelection = option[ruk];
 }
@@ -26,3 +52,32 @@ document.getElementById('compchoice').innerHTML = `${computerSelection}`;
 
 console.log(computerSelection);
 console.log(ruk);
+
+//play again
+function again() {
+  rockConst.classList.add('hidden');
+  paperConst.classList.add('hidden');
+  scissorConst.classList.add('hidden');
+}
+
+//Game
+function play(playerSelection, computerSelection) {
+  computerSelection = computerSelection.toLowerCase();
+  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection == computerSelection) {
+    displayy('Its a tie! close one');
+  } else {
+    displayy(' close one');
+  }
+}
+
+//resetGame
+function resetGame() {
+  computerScore = 0;
+  playerScore = 0;
+}
+
+//display
+function displayy(str) {
+  document.getElementById('resultt').innerHTML = str;
+}
